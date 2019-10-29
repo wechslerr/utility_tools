1331cac9a5f3674abfd8ef631a114382771643e4
Aqua <mutantneu@gmail.com>
2019-10-29 14:29:58 +0900

    Update setup_ubuntu18.04.sh

--- setup/setup_ubuntu18.04.sh
+++ setup/setup_ubuntu18.04.sh
@@ -106,9 +106,12 @@ cd libc-database
 
 cd $HOME
 
-# chsh -s /usr/bin/zsh
+echo alias gef="gdb --nh -ix ~/.gdbinit_gef" >> ~/.bashrc
+echo alias pwndbg="gdb --nh -ix ~/.gdbinit_pwndbg" >> ~/.bashrc
+echo alias peda="gdb --nh -ix ~/.gdbinit_peda" >> ~/.bashrc
 
-# you shoud write these in ~/.bashrc
+# if you use zsh, you shoud write these in ~/.zshrc
+# chsh -s /usr/bin/zsh
 
 << COMMENTOUT
 export PATH=$HOME/bin:$PATH
