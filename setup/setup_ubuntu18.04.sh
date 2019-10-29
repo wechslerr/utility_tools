f65c6d857a9e8fe3b889f90ec2c5258c77efb7e9
Aqua <mutantneu@gmail.com>
2019-10-29 14:47:38 +0900

    Update setup_ubuntu18.04.sh

--- setup/setup_ubuntu18.04.sh
+++ setup/setup_ubuntu18.04.sh
@@ -110,6 +110,8 @@ echo alias gef="gdb --nh -ix ~/.gdbinit_gef" >> ~/.bashrc
 echo alias pwndbg="gdb --nh -ix ~/.gdbinit_pwndbg" >> ~/.bashrc
 echo alias peda="gdb --nh -ix ~/.gdbinit_peda" >> ~/.bashrc
 
+source ~/.bashrc
+
 # if you use zsh, you shoud write these in ~/.zshrc
 # chsh -s /usr/bin/zsh
 
